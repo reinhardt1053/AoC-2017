60c0067bde448a6c62b8d956a30e08108d845b16
Massimo Fazzolari <reinhardt1053@gmail.com>
2017-12-07 00:34:59 +0100

    Indentation using tab

--- day2/src/main.cc
+++ day2/src/main.cc
@@ -13,18 +13,18 @@ int checksum1(int input[16][16]){
 	int min, max, value, checksum = 0;
 
 	for (int i = 0; i < 16; i++) {
-    	min = 100000;
-    	max = 0;
+		min = 100000;
+		max = 0;
 
-    	for (int j = 0; j < 16; j++){
-    		value = input[i][j];
+		for (int j = 0; j < 16; j++){
+			value = input[i][j];
 
-    		if (value < min) min = value;
-    		if (value > max) max = value;
-    	}
+			if (value < min) min = value;
+			if (value > max) max = value;
+		}
 
-    	checksum += (max-min);
-    }
+		checksum += (max-min);
+	}
 
 	return checksum;
 }
@@ -34,42 +34,42 @@ int checksum2(int input[16][16]){
 
 	for (int i = 0; i < 16; i++) {
 
-    	for (int j = 0; j < 16; j++){
-    		a = input[i][j];
-    		
-    		for (int z = 0; z < 16; z++){
-    			b = input[i][z];
-    			if (z != j && a % b == 0) {
-    				checksum += (a/b);
-    				break;
-    			}
-    		}
-    	}
-    }
+		for (int j = 0; j < 16; j++){
+			a = input[i][j];
+			
+			for (int z = 0; z < 16; z++){
+				b = input[i][z];
+				if (z != j && a % b == 0) {
+					checksum += (a/b);
+					break;
+				}
+			}
+		}
+	}
 
 	return checksum;
 }
 
 int main() {
 
-    ifstream file("input.txt");
-    
-    if (!file)
+	ifstream file("input.txt");
+	
+	if (!file)
 	{
-	    throw std::runtime_error("Cannot open input.txt");
+		throw std::runtime_error("Cannot open input.txt");
 	}
 
 	int input[16][16];
 	for (int i = 0; i < 16; i++) {
 
-    	for (int j = 0; j < 16; j++){
-    		file >> input[i][j];
-    	}
-    }
+		for (int j = 0; j < 16; j++){
+			file >> input[i][j];
+		}
+	}
 
-    cout << "Checksum 1: " << checksum1(input) << endl;
+	cout << "Checksum 1: " << checksum1(input) << endl;
 	cout << "Checksum 2: " << checksum2(input) << endl;
-    return 0;
+	return 0;
 }
 
 
